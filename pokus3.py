53ff466ca4eae9df41618955c6775d7b8c9f41cc
Tomáš Mareček <marecekt@ceps.cz>
2020-03-03 15:47:47 +0100

    12 commit

--- pokus3.py
+++ pokus3.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     print("Master-change-2")
     print("Master-change-3")
     print("Changed in Github.com")
+    print("First Change in Pycharm")
     print("Second Change in Github.com")
